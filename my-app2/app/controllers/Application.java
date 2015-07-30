1e0a5b230bc26cc1e4b09ceb551762a728f2fa0d
myapos <myapos@yahoo.com>
2015-07-30 21:04:35 +0300

    fix register form functionality 4
    fix register form functionality 4

--- my-app2/app/controllers/Application.java
+++ my-app2/app/controllers/Application.java
@@ -134,7 +134,7 @@ String globalusername="guest";
         /*session("username", loginForm.get().username);*/
         //return redirect(routes.Application.register(Form.form(RegisterUser.class), usr.username,usr.role));
         //return ok(views.html.register.render(Form.form(RegisterUser.class),usr.username,usr.role)); 
-        return ok(views.html.register.render(Form.form(RegisterUser.class),usr.username,usr.role)); 
+        return ok(views.html.register.render(registerForm,usr.username,usr.role)); 
     }
     
   }
