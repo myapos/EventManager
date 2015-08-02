230166c2a91b209383966639e3911409e0fe84df
myapos <myapos@yahoo.com>
2015-08-02 17:09:33 +0300

    test jquery ext script 6
    test jquery ext script 6

--- my-app2/public/javascripts/my.js
+++ my-app2/public/javascripts/my.js
@@ -1,20 +1,77 @@
 $(document).ready(function() {
 
 alert("Helo from external jquery script");
+var locations = [
+      ['Androgeo', 35.339409,25.1343932],
+      ['Kipotheatro Kazantzakis', 35.332105, 25.131702],
+      ['Pili Bithleem', 35.3345449,25.1277602],
+      ['Pili Bitouri', 35.334279,25.1347117],
+      ['Plateia Eleutherias', 35.3380048,25.1359151]
+    ];
+  
+    var myLatlng = new google.maps.LatLng(35.3220164, 25.1350708);
+	 var element = document.getElementById("map");
+ 
+	var map = new google.maps.Map(element, {
+ 	  center: myLatlng,
+ 	  zoom: 13,
+ 	  mapTypeId: "OSM",
+ 	  mapTypeControl: true,
+ 	  streetViewControl: true
+	 });
+	 
+    var infowindow = new google.maps.InfoWindow();
+    var marker, i;
+    var markers = new Array();
+    for (i = 0; i < locations.length; i++) {  
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        map: map
+      });
+      markers.push(marker);
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0]);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+    }
+    
+    function AutoCenter() {
+      //  Create a new viewpoint bound
+      var bounds = new google.maps.LatLngBounds();
+      //  Go through each...
+      $.each(markers, function (index, marker) {
+      bounds.extend(marker.position);
+      });
+      //  Fit these bounds to the map
+      map.fitBounds(bounds);
+    }
+    AutoCenter();
 
-var map = new ol.Map({
-    target: 'map',
-    layers: [
-      new ol.layer.Tile({
-        source: new ol.source.MapQuest({layer: 'sat'})
-      })
-    ],
-    view: new ol.View({
-      center: ol.proj.transform([37.41, 8.82], 'EPSG:4326', 'EPSG:3857'),
-      zoom: 4
-    })
-  });
+
+
+//Define OSM map type pointing at the OpenStreetMap tile server
+map.mapTypes.set("OSM", new google.maps.ImageMapType({
+  getTileUrl: function(coord, zoom) {
+  // "Wrap" x (logitude) at 180th meridian properly
+  /* NB: Don't touch coord.x because coord param is by reference, and changing its x property 
+  break's something in Google's lib */
+  var tilesPerGlobe = 1 << zoom;
+  var x = coord.x % tilesPerGlobe;
+  if (x < 0) {
+  x = tilesPerGlobe+x;
+  }
+  // Wrap y (latitude) in a like manner if you want to enable vertical infinite scroll
+  return "http://tile.openstreetmap.org/" + zoom + "/" + x + "/" + coord.y + ".png";
+  },
+ tileSize: new google.maps.Size(256, 256),
+ name: "OpenStreetMap",
+ maxZoom: 18
+}));
 alert("Helo from external jquery script");
+
+
 });
  
  
